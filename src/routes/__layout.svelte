9ab78742c477a6b6e3243c0f54c6bab4409cde25
levpen <levpen@yandex.ru>
2022-07-06 15:38:54 +0300

    Added components

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import '../styles/global.css';
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
+</script>
+
+<header>
+    <title>My website</title>
+    <Header/>
+</header>
+<main class="vertical-center">
+    <!-- page content -->
+    <slot></slot>
+</main>
+<footer>
+    <Footer/>
+</footer>
+
+<style>
+    * {
+		color: grey;
+		font-family:sans-serif;
+		font-size: 16px;
+		line-height: 1.6em;
+		margin:0;
+		text-align: center;
+	}
+    main{
+        margin-top: 20px;
+        
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <div class="container" id="about-main">
+        <h2>About Me</h2>
+        <div id="about-block">
+            <img src="/images/icon.png" alt="" id="icon" class="block">
+            <p class="block">Hello, I'm 18 y.o. freshmen in Innopolis Univerity who tries to find his passion in
+                programming.</p>
+        </div>
+    </div>
+</main>
+
+<style>
+    .block{
+        float:left;
+        width:50%;
+        margin-right: 15px;
+    }
+    #about-block{
+        padding-left: 25%;
+    }
+    #icon{
+        width: 85px;
+        border-style:solid;
+        border-radius: 50px;
+        border-color:rgb(41, 37, 37);
+    }
+</style>
