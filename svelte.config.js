520f3c3c48b720f88d1364bd1b817e8061aa221b
levpen <levpen@yandex.ru>
2022-07-06 23:17:42 +0300

    Changed config

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
+const dev = true;
 
-///** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-		base: dev ? '' : '/SvelteKitWebsite',
-	},
-	appDir: 'app_'
+		adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "/SvelteKitWebsite",
+        },
+        appDir: "_app"
+        // hydrate the <div id="svelte"> element in src/app.html
+	}
 };
 
 export default config;
