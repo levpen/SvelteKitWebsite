fe0b5b2114738169806ca2d69b55d5af95e28d24
levpen <levpen@yandex.ru>
2022-07-11 20:46:41 +0300

    added eslint and prettier

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,23 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
 
-const dev = true;
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter({
-            pages: "docs",
-            assets: "docs"
-        }),
-        paths: {
-            // change below to your repo name
-            base: dev ? "" : "/SvelteKitWebsite",
-        },
-        appDir: "_app"
-        // hydrate the <div id="svelte"> element in src/app.html
-	}
+  kit: {
+    adapter: adapter(),
+    serviceWorker: {
+      register: false,
+    },
+    paths: {
+      // base: dev ? '' : '/bulatok.github.io',
+    },
+    prerender: {
+      default: true,
+    },
+  },
 };
 
 export default config;
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
+
+console.log(import.meta.env);
 /** @type {import('vite').UserConfig} */
 const config = {
         plugins: [sveltekit()]
